2dc371b4981b85f6a522753b5655ea3a7a58e230
Lưu Hoàng Anh <anhlhse171415@fpt.edu.vn>
2026-03-30 23:36:41 +0700

    Thêm Api Dashboard tổng quan cho admin

--- src/main/java/swp/be/vn/bs/repository/WalletRepository.java
+++ src/main/java/swp/be/vn/bs/repository/WalletRepository.java
@@ -1,9 +1,11 @@
 package swp.be.vn.bs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import swp.be.vn.bs.entity.Wallet;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,10 @@ public interface WalletRepository extends JpaRepository<Wallet, Integer> {
     Optional<Wallet> findByUser_UserId(Integer userId);
 
     Optional<Wallet> findByUser_Email(String email);
+
+    @Query("SELECT COALESCE(SUM(w.balance), 0) FROM Wallet w")
+    BigDecimal sumAllBalances();
+
+    @Query("SELECT COALESCE(SUM(w.lockedBalance), 0) FROM Wallet w")
+    BigDecimal sumAllLockedBalances();
 }
